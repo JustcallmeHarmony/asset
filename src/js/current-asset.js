feae9f5665a326586f7d65c4e41156406219ef06
Joon <wnsduq1130naver.com@gimjun-yeob-ui-MacBookAir.local>
2024-01-15 00:45:16 +0900

    목록 조회 구현

--- src/js/current-asset.js
+++ src/js/current-asset.js
@@ -1,54 +1,57 @@
-import { addCurrentAsset } from "../api/add-current-asset";
-import { getCurrentAsset } from "../api/get-current-asset";
-import { toHidden, toShow } from "./util";
+import { addCurrentAsset } from "../api/add-current-asset.js";
+import { getCurrentAsset } from "../api/get-current-asset.js";
+import { toHidden, toShow } from "./util.js";
 
 const $currentAssetValue = document.querySelector(".current-asset-value");
 const $currentAssetLoader = document.querySelector(".current-asset-loader");
 const $currentAssetInput = document.querySelector(".current-asset-input");
 const $currentAssetButton = document.querySelector(".current-asset-button");
-const $currentAssetButtonLoader = document.querySelector(".add-item-button-loader");
-const $addItemButton = document.querySelector("add-item-button");
+const $currentAssetButtonLoader = document.querySelector(
+  ".current-asset-button-loader"
+);
+const $addItemButton = document.querySelector(".add-item-button");
 
 export const initCurrentAsset = () => {
-    handleCurrentAsset();
+  handleGetCurrentAsset();
 
-    $currentAssetButton.addEventListener('click', function() {
-        const inputValue = $currentAssetInput.ariaValueMax;
-        if(inputValue > 0 ) {
-            handleAddCurrentAsset();
-        } else {
-            console.warn("0원 이상이 아닙니다.");
-        }
-    });
-};
-
-const handleAddCurrentAsset = async () => {
-    toShow($currentAssetButtonLoader);
-    toHidden($currentAssetButton);
-
-    await addCurrentAsset(Number(inputValue)); 
-
-    toHidden($currentAssetButtonLoader);
-    toShow($currentAssetButton);
-};
-
-const handleCurrentAsset = async () => {
-    toShow($currentdAssetLoader)
-
-    try {
-        const {price} = await getCurrentAsset();
-        if(price > 0) {
-        $currentAssetValue.textContent = price.toLocalString();
-        toHidden($currentAssetButton);
-         } else { 
-        toShow($currentAssetInput);
-        toShow($currentAssetButton);
-        toHidden($addItemButton);
+  $currentAssetButton.addEventListener("click", function () {
+    const inputValue = $currentAssetInput.value;
+    if (inputValue > 0) {
+      handleAddCurrentAsset(inputValue);
+    } else {
+      console.warn("0원 이상이 아닙니다.");
     }
-    } catch (err) {
-        console.error("현재자산을 조회하는데 실패했습니다.");
-    }
-
-    toHidden($currentAssetLoader);
+  });
 };
 
+const handleAddCurrentAsset = async (inputValue) => {
+  toShow($currentAssetButtonLoader);
+  toHidden($currentAssetButton);
+
+  await addCurrentAsset(Number(inputValue));
+
+  toHidden($currentAssetButtonLoader);
+  toShow($currentAssetButton);
+
+  await handleGetCurrentAsset();
+};
+
+const handleGetCurrentAsset = async () => {
+  toShow($currentAssetLoader);
+
+  try {
+    const { price } = await getCurrentAsset();
+    if (price > 0) {
+      $currentAssetValue.textContent = price.toLocaleString();
+      toHidden($currentAssetInput);
+    } else {
+      toShow($currentAssetInput);
+      toShow($currentAssetButton);
+      toHidden($addItemButton);
+    }
+  } catch (err) {
+    console.error("현재자산을 조회한는데 실패했습니다.");
+  }
+
+  toHidden($currentAssetLoader);
+};
